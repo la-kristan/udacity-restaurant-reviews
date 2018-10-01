d8a5db7cedb5b3bb8f28796bb51f871db9bd002e
Hansberry <chransberry@gmail.com>
2018-09-30 21:12:31 -0600

    FEAT: Improve accessibility features

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -174,6 +174,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.ariaCurrent = "page";
   breadcrumb.appendChild(li);
 }
 
